a779f867110c2a1199ff99b12f46e021bf97b940
msistrunk <sistrunkmitchell@gmail.com>
2022-03-02 22:35:35 -0600

    ico

---
+++ sophomore/ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xfD0e04d830597a58C40384d1b3Efa9acE7E214ED";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
