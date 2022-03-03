a346468cf9db8ad34d84cb870e8e49cc989fe42b
msistrunk <sistrunkmitchell@gmail.com>
2022-03-02 23:33:07 -0600

    DAO

---
+++ sophomore/DAO/hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xfD0e04d830597a58C40384d1b3Efa9acE7E214ED";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
